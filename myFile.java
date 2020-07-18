119bf594657c9245f4415090ae39c5dbb2330603
SaiTeja-Ambari <61013612+SaiTeja-Ambari@users.noreply.github.com>
2020-07-18 21:36:38 +0530

    Update myFile.java

--- myFile.java
+++ myFile.java
@@ -1,22 +1,68 @@
 package Jaskaran.Collections;
 
+import java.util.AbstractList;
+import java.util.Arrays;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-public class App
+
+public class MyList<E> extends AbstractList<E>
 {
 	private static final Logger LOGGER = LogManager.getLogger(App.class);
-    public static void main(String[] args) {
-        MyList<Integer> ml = new MyList<Integer>();
-        for(int i = 0;i<15;i++)
+    private static final int INITIAL_CAPACITY = 10;
+    private int actSize = 0;
+    private Object elementData[];
+    private int i=0;
+    public MyList()
+    {
+        elementData=new Object[INITIAL_CAPACITY];
+        i=0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int index)
+    {
+        E x =(E)elementData[index];
+        return x;
+    }
+    public boolean add(E e)
+    {
+        elementData[i]=e;
+        i++;
+
+        if(i >= actSize)
         {
-            ml.add(i);
+            increaseListSize();
         }
-        ml.remove(3);
-        ml.remove(3);
-        LOGGER.info("Size of List :"+ml.size()+"");
-        LOGGER.info("Print List\n");
-        LOGGER.info(" List : \n");
-        ml.display();
+        return true;
+    }
+    @SuppressWarnings("unchecked")
+    public E remove(int index)
+    {
+        int j;
+        Object x=elementData[index];
+        for(j=index;j<actSize-1;j++)
+        {
+            elementData[j]=elementData[j+1];
+        }
+        i--;
+        actSize--;
+        return (E)x;
+    }
+    public int size()
+    {
+        return i;
+    }
+    private void increaseListSize()
+    {
+        actSize = elementData.length * 2;
+        elementData = Arrays.copyOf(elementData, actSize);
+    }
+    public void display()
+    {
+        String s="";
+        for (int i = 0; i < this.i; i++)
+            s=s+elementData[i]+" ";
+        LOGGER.info(s+"\n");
     }
 }
